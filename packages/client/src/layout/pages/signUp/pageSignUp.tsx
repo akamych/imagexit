8583e6649a4758f76175920443341ff48f91dfd7
Alexey "akamych" Kalinin <akamych@gmail.com>
2024-01-21 16:37:36 +0100

    no inline css

--- packages/client/src/layout/pages/signUp/pageSignUp.tsx
+++ packages/client/src/layout/pages/signUp/pageSignUp.tsx
@@ -36,7 +36,7 @@ export const PageSignUp = () => {
 
   const prefixSelector = (
     <Form.Item name="prefix" noStyle>
-      <Select style={{ width: 70 }}>
+      <Select>
         <Option value="7">+7</Option>
       </Select>
     </Form.Item>
@@ -53,7 +53,6 @@ export const PageSignUp = () => {
         initialValues={{
           prefix: '+7',
         }}
-        style={{ maxWidth: 600 }}
         scrollToFirstError>
         <Form.Item
           name="login"
@@ -114,7 +113,7 @@ export const PageSignUp = () => {
           name="phone"
           label="Номер телефона"
           rules={[{ required: true, message: 'Укажите ваш номер телефона!' }]}>
-          <Input addonBefore={prefixSelector} style={{ width: '100%' }} />
+          <Input addonBefore={prefixSelector} />
         </Form.Item>
 
         <Form.Item
