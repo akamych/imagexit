c8b76ae64d0b2b6f139868c01e21406c2fac46cf
Dan Gulkin <111271073+dangulkin@users.noreply.github.com>
2024-01-24 07:29:39 +0000

    Экран с началом игры

--- packages/client/src/layout/pages/main/pageMain.tsx
+++ packages/client/src/layout/pages/main/pageMain.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import { Button } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
-import styles from './styles.module.css';
+import { Button } from 'antd'
+import { Link } from 'react-router-dom'
+import styles from './styles.module.css'
 
 export const PageMain = () => {
   return (
-      <div className={styles.content}>
-          <img src='https://playimaginarium.cosmodrome.games/_nuxt/img/logo.ce33400.png'/>
-          <p>Онлайн версия популярной настольной игры.<br/>Начни играть прямо сейчас и погрузись в мир воображения!</p>
-          <Button className={styles.playBtn} type="primary">
-            Играть
-          </Button>
-      </div>
-  );
-};
+    <div className={styles.content}>
+      <img src="https://playimaginarium.cosmodrome.games/_nuxt/img/logo.ce33400.png" />
+      <p>
+        Онлайн версия популярной настольной игры.
+        <br />
+        Начни играть прямо сейчас и погрузись в мир воображения!
+      </p>
+      <Link to="/play">
+        <Button className={styles.playBtn} type="primary">
+          Играть
+        </Button>
+      </Link>
+    </div>
+  )
+}
