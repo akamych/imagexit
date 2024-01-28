4c5f3dac61b19f703309ea8077e9018cb44d085d
Dan Gulkin <111271073+dangulkin@users.noreply.github.com>
2024-01-28 17:17:24 +0000

    Обновил сервис воркер

--- packages/client/sw.js
+++ packages/client/sw.js
@@ -1,27 +1,38 @@
 const CACHE_NAME = 'app-cache-v1';
 const URLS = [
     '/',
-    '/public/assets/images/cards/1.jpeg',
-    '/public/assets/images/cards/2.jpeg',
-    '/public/assets/images/cards/3.jpeg',
-    '/public/assets/images/cards/4.jpeg',
-    '/public/assets/images/cards/5.jpeg',
-    '/public/assets/images/cards/6.jpeg',
-    '/src/main.tsx'
+    '/assets/images/cards/1.jpeg',
+    '/assets/images/cards/2.jpeg',
+    '/assets/images/cards/3.jpeg',
+    '/assets/images/cards/4.jpeg',
+    '/assets/images/cards/5.jpeg',
+    '/assets/images/cards/6.jpeg',
+    '/src/main.tsx',
+    '/src/index.css',
+    '/src/App.tsx',
+    '/src/App.css'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', async event => {
     console.log('install');
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(cache => {
-            console.log('Opened cache');
-            cache.addAll(URLS);
-        })
-        .catch(err => {
-            console.error(err); 
-            throw err;
-        })
-    )
+    try{
+        const cache = await caches.open(CACHE_NAME);
+        await cache.addAll(URLS);
+    }catch (err){
+        console.error(err); 
+        throw err;
+    }
+    
 });
+
 self.addEventListener('activate', event => console.log('activate')); 
+
+self.addEventListener('fetch', event => {
+    console.log('Fetch: ', event.request.url)
+    event.respondWith(cacheFirst(event.request))
+}); 
+
+async function cacheFirst(request){
+    const cached = await caches.match(request);
+    return cached ?? await fetch(request);
+}
