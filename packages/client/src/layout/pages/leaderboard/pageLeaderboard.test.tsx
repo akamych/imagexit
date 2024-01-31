226c3cfafcc7cb85360cb9f44e650fbe942afacb
kizin_d <dumuch@yandex.ru>
2024-01-31 10:55:30 +0800

    Созданы тесты на все страницы

---
+++ packages/client/src/layout/pages/leaderboard/pageLeaderboard.test.tsx
@@ -0,0 +1,31 @@
+import '@testing-library/jest-dom'
+import { render } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom' // Импортируйте BrowserRouter
+import { create } from 'match-media-mock'
+import { PageLeaderboard } from './pageLeaderboard'
+
+describe('PageLeaderboard component', () => {
+  beforeEach(() => {
+    window.matchMedia = create()
+  })
+
+  it('Компонент рендерится', () => {
+    const { container } = render(
+      <BrowserRouter>
+        <PageLeaderboard />
+      </BrowserRouter>
+    )
+    expect(container).toBeInTheDocument()
+  })
+
+  it('В компоненте присутствуют табы (TOP 10, Я в рейтинге)', () => {
+    const { container } = render(
+      <BrowserRouter>
+        <PageLeaderboard />
+      </BrowserRouter>
+    )
+
+    expect(container.innerHTML.indexOf('TOP 10') >= 0).toBe(true)
+    expect(container.innerHTML.indexOf('Я в рейтинге') >= 0).toBe(true)
+  })
+})
---
+++ packages/client/src/layout/pages/pageGame.test.tsx
@@ -0,0 +1,24 @@
+import '@testing-library/jest-dom'
+import { render } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom' // Импортируйте BrowserRouter
+import { create } from 'match-media-mock'
+import { PageGame } from './pageGame'
+import { setupJestCanvasMock } from 'jest-canvas-mock'
+
+describe('PageGame component', () => {
+  beforeEach(() => {
+    window.matchMedia = create()
+    jest.resetAllMocks()
+    setupJestCanvasMock()
+  })
+
+  it('Компонент рендерится', () => {
+    const { container } = render(
+      <BrowserRouter>
+        <PageGame />
+      </BrowserRouter>
+    )
+
+    expect(container).toBeInTheDocument()
+  })
+})
