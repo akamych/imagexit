50a5f815e1813c98a763d0a4268b804b420452ed
Dan Gulkin <111271073+dangulkin@users.noreply.github.com>
2024-02-01 15:08:11 +0000

    Отредактировал service worker

--- packages/client/service-worker.js
+++ packages/client/service-worker.js
@@ -1,13 +1,13 @@
 const CACHE_NAME = 'app-cache-v1';
 const URLS = [
     '/',
+    '/index.html',
     // '/assets/images/cards/1.jpeg',
     // '/assets/images/cards/2.jpeg',
     // '/assets/images/cards/3.jpeg',
     // '/assets/images/cards/4.jpeg',
     // '/assets/images/cards/5.jpeg',
     // '/assets/images/cards/6.jpeg',
-    '/index.html',
     // '/assets/images/logo.png',
     // '/assets/images/mascot.png',
     // '/assets/images/mainbg.jpg',
@@ -15,34 +15,39 @@ const URLS = [
 
 self.addEventListener('install', async event => {
     console.log('install');
-    try{
+    try {
         const cache = await caches.open(CACHE_NAME);
         await cache.addAll(URLS);
-    }catch (err){
-        console.error(err); 
+    } catch (err) {
+        console.error(err);
         throw err;
     }
-    
 });
 
-self.addEventListener('activate', event => console.log('activate')); 
+self.addEventListener('activate', () => console.log('activate'));
 
 self.addEventListener('fetch', async event => {
-    try{
-        // Смотрим, если запрос на файл из папки /assets
+    try {
+        console.log('Fetch: ', event.request.url);
+        // Check if the request is for an asset file in the /assets folder
         if (event.request.url.includes('/assets/')) {
-            return handleAssetRequest(event.request);
+            event.respondWith(handleAssetRequest(event.request));
+        } else {
+            event.respondWith(cacheFirst(event.request));
         }
-        // Для остальных запросов сначала смотрим в кэш
-        cacheFirst(event.request);
-    }catch(error){
+    } catch (error) {
         console.error('Fetch error:', error);
     }
-}); 
+});
 
-async function cacheFirst(request){
-    const cached = await caches.match(request);
-    return cached ?? await fetch(request);
+async function cacheFirst(request) {
+    try {
+        const cached = await caches.match(request);
+        return cached || fetch(request);
+    } catch (error) {
+        console.error('Cache first error:', error);
+        throw error;
+    }
 }
 
 async function handleAssetRequest(request) {
@@ -55,6 +60,7 @@ async function handleAssetRequest(request) {
         }
         return response;
     } catch (error) {
-        console.error('Ошибка обрабоки запроса в /assets:', error);
+        console.error('Asset request error:', error);
+        throw error;
     }
 }
