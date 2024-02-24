bd5a928a65169fe852c78ec9d7054966df453f4c
kizin_d <dumuch@yandex.ru>
2024-02-24 20:54:02 +0800

    fix test

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -3,11 +3,13 @@ import App from './App'
 import { act, render } from '@testing-library/react'
 import { create } from 'match-media-mock'
 import 'react-redux'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import React from 'react'
+import store from './store/Store'
 
 // @ts-ignore
-global.fetch = jest.fn(() =>
-  Promise.resolve({ json: () => Promise.resolve('hey') })
-)
+global.fetch = jest.fn(() => Promise.resolve({ json: () => Promise.resolve('hey') }))
 
 describe('App component', () => {
   beforeEach(() => {
@@ -16,7 +18,13 @@ describe('App component', () => {
 
   it('Компонент рендерится', async () => {
     await act(async () => {
-      const { container } = render(<App />)
+      const { container } = render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </Provider>
+      )
       expect(container).toBeInTheDocument()
     })
   })
