3ae98ced990fe3e0b27e0c3d9602a6d027a7aa10
Dan Gulkin <111271073+dangulkin@users.noreply.github.com>
2024-03-12 06:59:41 +0000

    deleted unnecessary code

--- packages/server/sequelize/index.ts
+++ packages/server/sequelize/index.ts
@@ -1,19 +1,5 @@
 import { Sequelize, SequelizeOptions } from 'sequelize-typescript'
-import * as path from 'path'
-import * as fs from 'fs'
-
 const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_PORT } = process.env
-
-// Получаем абсолютный путь к директории с моделями
-const modelsPath = path.join(__dirname, 'models')
-
-// Читаем все файлы в директории моделей
-const modelFiles = fs.readdirSync(modelsPath)
-
-// Фильтруем файлы моделей
-const models = modelFiles
-  .filter(file => file.endsWith('.ts') && file !== 'index.ts') // Исключаем файл index.ts (если он есть)
-  .map(file => require(path.join(modelsPath, file))) // Импортируем каждую модель
 
 const sequelizeOptions: SequelizeOptions = {
   dialect: 'postgres',
@@ -22,7 +8,7 @@ const sequelizeOptions: SequelizeOptions = {
   username: POSTGRES_USER,
   password: POSTGRES_PASSWORD,
   database: POSTGRES_DB,
-  models: models,
+  models: [__dirname + '/models'],
 }
 
 const sequelize = new Sequelize(sequelizeOptions)
