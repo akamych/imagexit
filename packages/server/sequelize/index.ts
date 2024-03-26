619c7f46b73a101837ea16e996701718cb89a183
kizin_d <dumuch@yandex.ru>
2024-03-26 13:23:50 +0800

    f

--- packages/server/sequelize/index.ts
+++ packages/server/sequelize/index.ts
@@ -1,8 +1,9 @@
+import * as pg from 'pg'
 import { Sequelize, SequelizeOptions } from 'sequelize-typescript'
 const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_PORT, POSTGRES_HOST } = process.env
 
 const sequelizeOptions: SequelizeOptions = {
-  dialect: 'postgres',
+  dialectModule: pg,
   host: POSTGRES_HOST,
   port: Number(POSTGRES_PORT),
   username: POSTGRES_USER,
