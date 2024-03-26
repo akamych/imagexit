9b362c45c143bb9f4bd1e6009f988c796e4e3508
Marina Lebedeva <marina.j.lebedeva@gmail.com>
2024-03-26 14:46:42 +0100

    add CSP

--- packages/client/src/cspMiddleware.ts
+++ packages/client/src/cspMiddleware.ts
@@ -3,11 +3,26 @@ import type { IncomingMessage, ServerResponse } from 'http'
 type Middleware = (req: IncomingMessage, res: ServerResponse, next: () => void) => void
 
 const cspMiddleware: Middleware = (req, res, next) => {
+  const domain = process.env.DOMAIN
+  // const clientPort = process.env.CLIENT_PORT
+  const serverPort = process.env.SERVER_PORT
   res.setHeader(
     'Content-Security-Policy',
-    `default-src 'self' process.env.SERVER_PORT; script-src 'self' 'unsafe-inline' process.env.SERVER_PORT; style-src 'self' 'unsafe-inline' process.env.SERVER_PORT; img-src 'self' process.env.SERVER_PORT data:; font-src 'self' process.env.SERVER_PORT data:; connect-src 'self' process.env.SERVER_PORT; frame-ancestors 'self' process.env.SERVER_PORT;`
+    `default-src 'self' data: ${domain}:${serverPort};
+     script-src 'self' 'unsafe-inline' ${domain}:${serverPort};
+     style-src 'self' 'unsafe-inline';
+     img-src 'self';
+     font-src 'self';
+     connect-src 'self' ${domain}:${serverPort}`
   )
   next()
 }
-
+/**
+default-src 'self' data: ${domain}:${serverPort} - разрешает загрузку контента с текущего домена и порта клиента + загрузку скриптов и других ресурсов с ${domain}:${serverPort}, а также использование application/json в качестве типа контента.
+script-src 'self' 'unsafe-inline' ${domain}:${serverPort}  - разрешает загрузку скриптов с текущего домена и порта клиента + встроенных скриптов (с помощью 'unsafe-inline') + использование application/json в качестве типа контента.
+style-src 'self' 'unsafe-inline'  - разрешает загрузку стилей с текущего домена и порта клиента, а также встроенных стилей (с помощью 'unsafe-inline').
+img-src 'self'  - разрешает загрузку изображений с текущего домена и порта клиента, а также из данных (с помощью data:).
+font-src 'self'   - разрешает загрузку шрифтов с текущего домена и порта клиента 
+connect-src 'self' ${domain}:${serverPort} - разрешает запросы к ${domain}:${serverPort} через WebSocket, EventSource, XMLHttpRequest и Fetch API.
+ */
 export default cspMiddleware
